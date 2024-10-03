76b0ae80eb29e245fafdc3a71d14e22fe0a8d449
PolyakGG <separik8@gmail.com>
2024-10-03 10:15:53 +0400

    done

---
+++ Answers/7/gadget/tape.go
@@ -0,0 +1,29 @@
+package gadget
+
+import "fmt"
+
+type TapePlayer struct {
+	batteries string
+}
+
+func (t TapePlayer) Play(song string) {
+	fmt.Println("Playing", song)
+}
+func (t TapePlayer) Stop() {
+	fmt.Println("Stopped!")
+}
+
+type TapeRecorder struct {
+	Microphones int
+}
+
+func (t TapeRecorder) Play(song string) {
+	fmt.Println("Playing", song)
+}
+
+func (t TapeRecorder) Record() {
+	fmt.Println("Recording")
+}
+func (TapeRecorder) Stop() {
+	fmt.Println("Stopped!")
+}
